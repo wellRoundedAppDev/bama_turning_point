d54657ef00817aa8f93e3510a0730fa1e68fede3
sonoma <noornido@gmail.com>
2024-11-21 12:53:01 +0200

    Nov 20 2024

--- lib/contact_us/presentation/cubit/contact_us_cubit.dart
+++ lib/contact_us/presentation/cubit/contact_us_cubit.dart
@@ -3,6 +3,7 @@ import 'package:classic_eccomerce/contact_us/data/models/contact_us_model.dart';
 import 'package:classic_eccomerce/contact_us/presentation/cubit/contact_us_states.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:mailer/mailer.dart';
 import 'package:mailer/mailer.dart';
 import 'package:mailer/mailer.dart';
@@ -29,32 +30,37 @@ class ContactUsCubit extends Cubit<ContactUsStates> {
     }
 
     contactUsFormKey.currentState?.save();
-    final smtpServer = smtp.gmail('knowmore19965@gmail.com', 'knowmorea7aba7a');
-
-    // Create the message
-    final message = Message()
-      ..from = const Address('knowmore19965@gmail.com', 'noor')
-      ..recipients.add('wellroundedappdev@gmail.com') // Recipient email
-      ..subject = 'Test Email from Flutter App'
-      ..text = 'This is a plain text email sent from a Flutter app.'
-      ..html = '<h1>This is a Flutter Email</h1><p>Sent via SMTP.</p>';
-
-    try {
-      final sendReport = await send(message, smtpServer);
-      print('Email sent: ${sendReport.toString()}');
-    } on MailerException catch (e) {
-      print('Email not sent: ${e.toString()}');
-    }    // final Email email = Email(
-    //     subject: contactUsModel.subject,
-    //     body: contactUsModel.name +
-    //         " - " +
-    //         contactUsModel.mobileNumber +
-    //         '\n' +
-    //         contactUsModel.message,
-    //     recipients: ["buqchashop2@gmail.com"],
-    //     isHTML: false);
+    // final smtpServer = smtp.gmail('knowmore19965@gmail.com', 'knowmorea7aba7a');
     //
-    // await FlutterEmailSender.send(email);
+    // // Create the message
+    // final message = Message()
+    //   ..from = const Address('knowmore19965@gmail.com', 'noor')
+    //   ..recipients.add('wellroundedappdev@gmail.com') // Recipient email
+    //   ..subject = 'Test Email from Flutter App'
+    //   ..text = 'This is a plain text email sent from a Flutter app.'
+    //   ..html = '<h1>This is a Flutter Email</h1><p>Sent via SMTP.</p>';
+    //
+    // try {
+    //   final sendReport = await send(message, smtpServer);
+    //   print('Email sent: ${sendReport.toString()}');
+    // } on MailerException catch (e) {
+    //   print('Email not sent: ${e.toString()}');
+    // }    // final Email email = Email(
+    // //     subject: contactUsModel.subject,
+    // //     body: contactUsModel.name +
+    // //         " - " +
+    // //         contactUsModel.mobileNumber +
+    // //         '\n' +
+    // //         contactUsModel.message,
+    // //     recipients: ["buqchashop2@gmail.com"],
+    // //     isHTML: false);
+    // //
+    await FlutterEmailSender.send(Email(subject: contactUsModel.subject,
+    body: "Hello, I am ${contactUsModel.name}, phone number ${contactUsModel?.mobileNumber}\n${contactUsModel?.message}",
+    recipients: [
+      "buqchashop2@gmail.com"
+    ]
+    ));
   }
 
 
