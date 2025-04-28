d8f1eed6c79aff9acdeead4dba40422939e2286c
noornido <noornido@gmail.com>
2025-04-28 17:26:59 +0300

    apr 28 2025

--- lib/core/constants/server_urls_and_keys/api_urls.dart
+++ lib/core/constants/server_urls_and_keys/api_urls.dart
@@ -88,6 +88,8 @@ class ApiUrls {
   static String getProductsInBrandEndpoint(int id) =>
       "$GET_PRODUCTS_IN_BRAND_ENDPOINT{$id}";
 
+  static const GET_ALL_NOTIFICATIONS_ENDPOINT = "api/SendNotifications/GetAllSelectList";
+
   //used products
 
   static const ADD_USED_PRODUCT_ENDPOINT =
--- lib/notifications/presentation/cubit/notifications_states.dart
+++ lib/notifications/presentation/cubit/notifications_states.dart
@@ -3,3 +3,12 @@ abstract class NotificationsStates{}
 class NotificationsInitialState extends NotificationsStates{}
 
 class NotificationsReceivedState extends NotificationsStates{}
+
+
+class GetAllNotificationsLoadingState extends NotificationsStates{}
+
+class GetAllNotificationsSuccessState extends NotificationsStates{}
+
+class GetAllNotificationsFailedState extends NotificationsStates{}
+
+class GetAllNotificationsNwConnectionFailedState extends NotificationsStates{}
